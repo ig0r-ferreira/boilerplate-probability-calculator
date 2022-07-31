9e25601f7e4a9be5138e0dc95aa67c5f854d212f
Igor Ferreira <igor-freitas10@hotmail.com>
2022-07-31 19:39:15 -0300

    Complete project

--- prob_calculator.py
+++ prob_calculator.py
@@ -3,6 +3,22 @@ import random
 # Consider using the modules imported above.
 
 class Hat:
+    def __init__(self, **kwargs) -> None:
+        self.contents = [content for key, value in kwargs.items() for content in [key] * value]
+        
+    def draw(self, num_balls_drawn:int) -> list[str]:
+        if num_balls_drawn > len(self.contents):
+            choices = self.contents
+            self.contents = []
+        else:
+            choices = [self.contents.pop(random.randrange(len(self.contents))) for _ in range(num_balls_drawn)]
+            
+        return choices
 
-
-def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+def experiment(hat:Hat, expected_balls:dict[str, int], num_balls_drawn:int, num_experiments:int) -> float:
+    
+    passed = lambda result: not any(result.count(color) < quant for color, quant in expected_balls.items())
+    hits = sum(passed(copy.deepcopy(hat).draw(num_balls_drawn)) for _ in range(num_experiments))
+    
+    return hits / num_experiments
+    
