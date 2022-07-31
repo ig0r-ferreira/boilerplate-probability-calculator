644f198630c72936def6e294b9454dbb2a6c58b3
Igor Ferreira <igor-freitas10@hotmail.com>
2022-07-31 19:49:59 -0300

    Fix annotations

--- prob_calculator.py
+++ prob_calculator.py
@@ -6,7 +6,7 @@ class Hat:
     def __init__(self, **kwargs) -> None:
         self.contents = [content for key, value in kwargs.items() for content in [key] * value]
         
-    def draw(self, num_balls_drawn:int) -> list[str]:
+    def draw(self, num_balls_drawn:int) -> 'list[str]':
         if num_balls_drawn > len(self.contents):
             choices = self.contents
             self.contents = []
@@ -15,10 +15,9 @@ class Hat:
             
         return choices
 
-def experiment(hat:Hat, expected_balls:dict[str, int], num_balls_drawn:int, num_experiments:int) -> float:
+def experiment(hat:Hat, expected_balls:'dict[str, int]', num_balls_drawn:int, num_experiments:int) -> float:
     
     passed = lambda result: not any(result.count(color) < quant for color, quant in expected_balls.items())
     hits = sum(passed(copy.deepcopy(hat).draw(num_balls_drawn)) for _ in range(num_experiments))
     
     return hits / num_experiments
-    
